c41192e516451a38aa286d859a0600e7f0b4baab
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-02-06 19:46:41 +0900

    fix bugs

--- models/sepformer.py
+++ models/sepformer.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
-from ..configure import configure
+import configure
 import torchaudio
 
 class PositionEncoding(nn.Module):
