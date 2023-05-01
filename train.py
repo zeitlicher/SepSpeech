1c3e1ce887874f9572594fbda3c862c6b01b17c1
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-05-01 10:26:48 +0900

    fix bugs

--- train.py
+++ train.py
@@ -1,15 +1,10 @@
-import torch
-import torch.utils.data as data
-import pytorch_lightning as pl
+import lightning.pytorch as pl
+from lightning.pytorch.loggers import TensorBoardLogger
 from lite.solver import LitSepSpeaker
-import conventional
 from conventional.speech_dataset import SpeechDataset
 import models.sepformer
 from argparse import ArgumentParser
 import yaml
-import warnings
-
-warnings.filterwarnings('ignore')
 
 '''
  PyTorch Lightning用 将来変更する予定
@@ -20,28 +15,27 @@ def main(config:dict, checkpoint_path=None):
         model = LitSepSpeaker.load_from_checkpoint(checkpoint_path, config=config)
     else:
         model = LitSepSpeaker(config)
-    
-    train_dataset = SpeechDataset(config['dataset']['train'],
-                                  config['dataset']['train_enroll'],
-                                  segment=config['train']['segment'])
+
+    train_dataset = SpeechDataset(**config['dataset']['train'], 
+                                  **config['dataset']['segment'])
     train_loader = data.DataLoader(dataset=train_dataset,
-                                   batch_size=config['train']['batch_size'], num_workers=2, pin_memory=True,
-                                   shuffle=True, collate_fn=lambda x: conventional.speech_dataset.data_processing(x))
-    valid_dataset = SpeechDataset(config['dataset']['valid'],
-                                  config['dataset']['valid_enroll'],
-                                  segment=config['train']['segment'])
+                                   **config['dataset']['process'],
+                                   pin_memory=True,
+                                   shuffle=True, 
+                                   collate_fn=lambda x: conventional.speech_dataset.data_processing(x))
+    valid_dataset = SpeechDataset(**config['dataset']['valid'],
+                                  **config['dataset']['segment'])
     valid_loader = data.DataLoader(dataset=valid_dataset,
-                                   batch_size=config['train']['batch_size'], num_workers=2, pin_memory=True,
-                                   shuffle=True, collate_fn=lambda x: conventional.speech_dataset.data_processing(x))
-    trainer = pl.Trainer(
-        accelerator='auto',
-        accumulate_grad_batches=config['train']['accumulate_grad_batches'],
-        default_root_dir=config['train']['default_root_dir'],
-        max_epochs=config['train']['max_epochs'],
-        precision=config['train']['precision'],
-        profiler='simple',
-    )
-    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+                                   **config['dataset']['process'],
+                                   pin_memory=True,
+                                   shuffle=False, 
+                                   collate_fn=lambda x: conventional.speech_dataset.data_processing(x))
+    callbacks = [
+        pl.callbacks.ModelCheckpoint( **config['checkpoint'])
+    ]
+    logger = TensorBoardLogger(**config['logger'])
+    trainer = pl.Trainer( callbacks=callbacks, **config['trainer'] )
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader, logger=logger)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
